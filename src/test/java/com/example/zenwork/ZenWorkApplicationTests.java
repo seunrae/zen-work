75e7c005304f4e8f01509d8f82bc8a47868ce64e
seunrae <ademilua.oluseun@gmail.com>
2024-05-13 14:01:08 +0100

    first commit

---
+++ src/test/java/com/example/zenwork/ZenWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zenwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
